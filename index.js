6f3faa1ff562e6311d8d71c25a5571fc526cf3cb
Anastasiya Staratsitarava <starotitorova.anastasia@mail.ru>
2024-07-09 11:51:34 +0300

    tenth commit

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-(function () {
-  const tryButton = document.getElementById('tryButton');
-  const favDialog = document.getElementById('favDialog');
-  const exitButton = document.getElementById('exit');
-  tryButton.addEventListener('click', function () {
-    favDialog.showModal();
-  });
-  exitButton.addEventListener('click', function () {
-    favDialog.close();
-  });
-})();
+const tryButton = document.getElementById('tryButton');
+const favDialog = document.getElementById('favDialog');
+const exitButton = document.getElementById('exit');
+tryButton.addEventListener('click', function () {
+  favDialog.showModal();
+});
+exitButton.addEventListener('click', function () {
+  favDialog.close();
+});
